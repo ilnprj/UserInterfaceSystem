6bdbc9fffb577ac856ae4e46fa3b4b6beda0a8cc
iln <romanhvorostov87@gmail.com>
2019-07-11 00:24:12 +0700

    Base Drag Drop

---
+++ Assets/Scripts/Extentsions/DragDrop/DragDropItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class DragDropItem : EventTrigger
+{
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+       
+    }
+}
