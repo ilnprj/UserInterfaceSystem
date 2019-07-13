f42bd4add6f88f0e5eb6738e8013cd112ef8c89d
iln <romanhvorostov87@gmail.com>
2019-07-13 17:03:25 +0700

    Рефакторинг иерархии объектов. Добавление возможности смены

---
+++ Assets/Scripts/MainController/Interfaces/ILoadableWindow.cs
@@ -0,0 +1,7 @@
+﻿/// <summary>
+/// Интерфейс, определяющий как будут загружаться окна в память
+/// </summary>
+public interface ILoadableWindow
+{
+    WindowAsset GetWindowAsset(string idWindow);
+}
